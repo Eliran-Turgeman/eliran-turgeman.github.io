d30a1af1fb7e54e93d783ecf390aee66ed4cc390
Eliran <eliran9692@gmail.com>
2026-08-07 13:42:09 +0300

    Site updated: 2026-08-07 13:42:09

--- js/main.js
+++ js/main.js
@@ -26,7 +26,6 @@ $(document).ready(function() {
    */
   if ($(".post").length) {
     var menu = $("#menu");
-    var nav = $("#menu > #nav");
     var menuIcon = $("#menu-icon, #menu-icon-tablet");
 
     /**
@@ -52,18 +51,11 @@ $(document).ready(function() {
     });
 
     /**
-     * Add a scroll listener to the menu to hide/show the navigation links.
+     * Swap the tablet menu icon for a scroll-to-top icon after scrolling.
      */
     if (menu.length) {
       $(window).on("scroll", function() {
         var topDistance = menu.offset().top;
-
-        // hide only the navigation links on desktop
-        if (!nav.is(":visible") && topDistance < 50) {
-          nav.show();
-        } else if (nav.is(":visible") && topDistance > 100) {
-          nav.hide();
-        }
 
         // on tablet, hide the navigation icon as well and show a "scroll to top
         // icon" instead
